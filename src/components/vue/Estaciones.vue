0dc07cce086311cb6735ba8599bd6288ddd3cb8f
QUEN97 <Antonio1308_97@outlook.com>
2024-05-09 12:27:08 -0600

    ligero update a ubicaciones

--- src/components/vue/Estaciones.vue
+++ src/components/vue/Estaciones.vue
@@ -1,55 +1,72 @@
 <script setup>
 import { guatemala } from "../../constants/guatemala";
 import estacionesService from "../../services/estacionesService";
-    import { ref,onMounted, computed } from 'vue'
-    const estados = ref([]);
-    const guatemalaEstaciones = ref([]);
-    const estadoSelected = ref('*');
-    onMounted(() => {
-        const lib= new estacionesService();
-        estaciones.value = lib.orderByCountryName();
-        estados.value = lib.getEstados();
-        guatemalaEstaciones.value = guatemala;
-    });
-    //propiedades computadas
-    const estaciones = computed(()=>{
-        const lib= new estacionesService();
-        if(estadoSelected.value=='*'){
-            return lib.orderByCountryName();
-        }else{
-            return lib.filterByCountryOrdered(estadoSelected.value);
-        }
-    });
+import { ref, onMounted, computed } from 'vue'
+const estados = ref([]);
+const guatemalaEstaciones = ref([]);
+const estadoSelected = ref('*');
+onMounted(() => {
+    const lib = new estacionesService();
+    estaciones.value = lib.orderByCountryName();
+    estados.value = lib.getEstados();
+    guatemalaEstaciones.value = guatemala;
+});
+//propiedades computadas
+const estaciones = computed(() => {
+    const lib = new estacionesService();
+    if (estadoSelected.value == '*') {
+        return lib.orderByCountryName();
+    } else {
+        return lib.filterByCountryOrdered(estadoSelected.value);
+    }
+});
 </script>
 <template>
-    <section id="mx"class="max-w-7xl m-auto mt-3 py-3 transition-[height] duration-300">
-        <h1 class="border-b border-red-200 text-red-200 pb-1.5 mb-3 font-bold text-4xl text-center">México</h1>
+    <section id="mx" class="max-w-7xl m-auto mt-3 py-3 transition-[height] duration-300">
+        <div class="flex items-center justify-center mb-3">
+            <h1 class="border-b border-red-500 text-red-500 pb-1.5 mb-3 font-bold text-4xl text-center">México</h1>
+            <img src="src/img/banderas/mexico.jfif" alt="Descripción de la imagen" class="ml-4"
+                style="max-width: 100px;">
+        </div>
         <div class="flex flex-wrap gap-2 items-center justify-center mb-5">
             <label for="all">
                 <input type="radio" name="estado" id="all" value="*" v-model="estadoSelected" class="hidden peer">
-                <span class="block cursor-pointer transition-colors duration-300 hover:bg-slate-700 hover:text-blue-50 peer-checked:bg-blue-800 peer-checked:border-blue-800 peer-checked:text-blue-50 font-semibold border border-blue-900 text-gray-400 px-2 py-1 rounded-full">Todos</span>
+                <span
+                    class="block cursor-pointer transition-colors duration-300 hover:bg-slate-700 hover:text-red-50 peer-checked:bg-red-800 peer-checked:border-red-800 peer-checked:text-red-50 font-semibold border border-red-900 text-gray-400 px-2 py-1 rounded-full">Todos</span>
             </label>
             <label v-for="estado in estados" :key="estado" :for="estado">
-                <input type="radio" name="estado" :id="estado" :value="estado" v-model="estadoSelected" class="hidden peer">
-                <span class="block cursor-pointer transition-colors duration-300 hover:bg-slate-700 hover:text-blue-50 peer-checked:bg-blue-800 peer-checked:border-blue-800 peer-checked:text-blue-50 font-semibold border border-blue-900 text-gray-400 px-2 py-1 rounded-full">{{estado}}</span>
+                <input type="radio" name="estado" :id="estado" :value="estado" v-model="estadoSelected"
+                    class="hidden peer">
+                <span
+                    class="block cursor-pointer transition-colors duration-300 hover:bg-slate-700 hover:text-red-50 peer-checked:bg-red-800 peer-checked:border-red-800 peer-checked:text-red-50 font-semibold border border-neutral-300 text-gray-400 px-2 py-1 rounded-full">{{
+                        estado }}</span>
             </label>
         </div>
         <div class="flex flex-wrap justify-center gap-3 max-h-96 overflow-y-auto">
-            <div v-for="(estacion,key) in estaciones" :key="key" class="w-full mx-2 sm:mx-0 sm:max-w-72 sm:min-w-64">
-                <a :href="estacion.url" target="_blank" class="bg-slate-700 text-slate-300 flex flex-col w-full h-full px-3 py-2 rounded-md hover:shadow-md hover:scale-110 transition-all duration-300">
-                    <h2 class="font-semibold mb-1">{{estacion.name}}</h2>
+            <div v-for="(estacion, key) in estaciones" :key="key" class="w-full mx-2 sm:mx-0 sm:max-w-72 sm:min-w-64">
+                <a :href="estacion.url" target="_blank"
+                    class="bg-neutral-300 text-neutral-700 flex flex-col w-full h-full px-3 py-2 rounded-md hover:shadow-lg hover:scale-110 transition-all duration-300">
+                    <h2 class="font-semibold mb-1">{{ estacion.name }}</h2>
                     <div class="flex flex-col justify-end gap-1 h-full text-xs">
                         <div class="w-full flex gap-2 items-center h-full">
-                            <div class="text-orange-600">
+                            <div class="text-red-500">
                                 <svg class="size-5" fill="currentColor" viewBox="0 0 384 512">
-                                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                                    <path
+                                        d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
                                 </svg>
                             </div>
-                            <p class="text-balance text-gray-400">{{estacion.direccion}}</p>
+                            <p class="text-balance text-neutral-700">{{ estacion.direccion }}</p>
                         </div>
                         <div class="w-full flex gap-3 text-cyan-600 items-center h-fit">
-                            <svg  class="size-4"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7.5" /><path d="M9 4v13" /><path d="M15 7v5.5" /><path d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z" /><path d="M19 18v.01" />
+                            <svg class="size-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7.5" />
+                                <path d="M9 4v13" />
+                                <path d="M15 7v5.5" />
+                                <path
+                                    d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z" />
+                                <path d="M19 18v.01" />
                             </svg>
                             <span>{{ estacion.estado }}</span>
                         </div>
@@ -60,24 +77,38 @@ import estacionesService from "../../services/estacionesService";
     </section>
     <br>
     <!-- --------------------------GUATEMALA------------------------------------- -->
-    <section id="gt"class="max-w-7xl m-auto mt-3 py-3 transition-[height] duration-300">
-        <h1 class="border-b border-red-200 text-red-200 pb-1.5 mb-3 font-bold text-4xl text-center">Guatemala</h1>
+    <section id="gt" class="max-w-7xl m-auto mt-3 py-3 transition-[height] duration-300">
+        <div class="flex items-center justify-center mb-3">
+            <h1 class="border-b border-red-500 text-red-500 pb-1.5 mb-3 font-bold text-4xl text-center">Guatemala</h1>
+            <img src="src/img/banderas/guatemala.jfif" alt="Descripción de la imagen" class="ml-4"
+                style="max-width: 100px;">
+        </div>
         <div class="flex flex-wrap justify-center gap-3 max-h-96 overflow-y-auto">
-            <div v-for="(estacion,key) in guatemalaEstaciones" :key="key" class="w-full mx-2 sm:mx-0 sm:max-w-72 sm:min-w-64">
-                <a :href="estacion.url" target="_blank" class="bg-slate-700 text-slate-300 flex flex-col w-full h-full px-3 py-2 rounded-md hover:shadow-md hover:scale-110 transition-all duration-300">
-                    <h2 class="font-semibold mb-1">{{estacion.name}}</h2>
+            <div v-for="(estacion, key) in guatemalaEstaciones" :key="key"
+                class="w-full mx-2 sm:mx-0 sm:max-w-72 sm:min-w-64">
+                <a :href="estacion.url" target="_blank"
+                    class="bg-neutral-300 text-neutral-700 flex flex-col w-full h-full px-3 py-2 rounded-md hover:shadow-md hover:scale-110 transition-all duration-300">
+                    <h2 class="font-semibold mb-1">{{ estacion.name }}</h2>
                     <div class="flex flex-col justify-end gap-1 h-full text-xs">
                         <div class="w-full flex gap-2 items-center h-full">
-                            <div class="text-orange-600">
+                            <div class="text-red-500">
                                 <svg class="size-5" fill="currentColor" viewBox="0 0 384 512">
-                                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                                    <path
+                                        d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
                                 </svg>
                             </div>
-                            <p class="text-balance text-gray-400">{{estacion.direccion}}</p>
+                            <p class="text-balance text-neutral-700">{{ estacion.direccion }}</p>
                         </div>
                         <div class="w-full flex gap-3 text-cyan-600 items-center h-fit">
-                            <svg  class="size-4"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7.5" /><path d="M9 4v13" /><path d="M15 7v5.5" /><path d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z" /><path d="M19 18v.01" />
+                            <svg class="size-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7.5" />
+                                <path d="M9 4v13" />
+                                <path d="M15 7v5.5" />
+                                <path
+                                    d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z" />
+                                <path d="M19 18v.01" />
                             </svg>
                             <span>{{ estacion.estado }}</span>
                         </div>

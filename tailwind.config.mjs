671f8a15bc9f6dd42f9a1b1ae14a42189e389cf6
QUEN97 <Antonio1308_97@outlook.com>
2024-05-14 16:38:33 -0600

    Ajustes seccion  noticias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				before: "-10px 10px 0 #0f172a",
+				before2: "-10px -10px 0 #0f172a",
+				after: "-10px 10px 0 #0f172a",
+				after2: "-10px 10px 0 #232949",
+			},
+			colors: {
+				primary: "#232949",
+			}
+		},
 	},
 	plugins: [],
 }

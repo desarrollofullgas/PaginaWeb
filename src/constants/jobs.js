05a2ada0f6502ab32e39b4a8a474d1f209814902
JoelEstrella06 <joelestrella06@gmail.com>
2024-05-31 11:28:46 -0600

    sección de bolsa

---
+++ src/constants/jobs.js
@@ -0,0 +1,40 @@
+export const jobs =[
+    {
+        tittle:'Capturista de datos',
+        location:'Mérida,Yuc.',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.',
+        salary:1000,
+        type:'Tiempo completo',
+        skills:['sk1','sk2','sk3','sk4','sk5'],
+        extraSkills:['exSk1','exSk2','exSk3','exSk4'],
+        coment:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi.'
+    },{
+        tittle:'Gerente de estación',
+        location:'Mérida,Yuc.',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.',
+        salary:1000,
+        type:'Medio completo',
+        skills:['sk1','sk2','sk3','sk4','sk5','sk6'],
+        extraSkills:['exSk1','exSk2','exSk3','exSk4'],
+        coment:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi.'
+    },{
+        tittle:'Auxiliar de sistemas',
+        location:'Mérida,Yuc.',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi. Dolore quis minus expedita, et laborum sapiente.',
+        salary:'1,000',
+        type:'Tiempo completo',
+        skills:['sk1','sk2','sk3','sk4'],
+        extraSkills:['exSk1','exSk2','exSk3','exSk4'],
+        coment:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa natus totam quidem eum illo, minus aut placeat, ad commodi, impedit sed sequi.'
+    },
+];
+/* {
+    tittle:'',
+    location:'',
+    description:'',
+    salary:0,
+    type:'
+    skills:[],
+    extraSkills:[],
+    coment:'
+}, */
